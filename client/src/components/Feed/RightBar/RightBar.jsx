ba87be85afa41238156343de2b136ad09ec71e2f
George <g.m.6@live.com>
2024-04-08 21:54:16 +0300

    following client

--- client/src/components/Feed/RightBar/RightBar.jsx
+++ client/src/components/Feed/RightBar/RightBar.jsx
@@ -4,6 +4,29 @@ import { useNavigate } from 'react-router-dom';
 
 const RightBar = ({ currentUser }) => {
     const navigate = useNavigate();
+
+    const SuggestedCard = ({ user }) => {
+        return (
+            <div className="right-bar-suggested flex space-between">
+                <div className="suggested-card flex align-center">
+                    <img
+                        src={
+                            user.image
+                                ? `http://127.0.0.1:8000/profile-pictures/${user.image}`
+                                : './images/assets/avatar.png'
+                        }
+                        alt="avatar"
+                    />
+                    <div className="suggested-card-text">
+                        <p className="black-text size-m bold">{user.username}</p>
+                        <p className="light-text size-m">Suggested for you</p>
+                    </div>
+                    <button className="follow-btn primary-text no-bg">Follow</button>
+                </div>
+            </div>
+        );
+    };
+
     return (
         <div className="right-bar-main">
             <div className="right-bar-profile flex align-center">
@@ -20,16 +43,6 @@ const RightBar = ({ currentUser }) => {
                 </p>
             </div>
             <p className="light-text size-m bold">Suggested for you</p>
-            <div className="right-bar-suggested flex space-between">
-                <div className="suggested-card flex align-center">
-                    <img src="./images/assets/avatar.png" alt="avatar" />
-                    <div className="suggested-card-text">
-                        <p className="black-text size-m bold">Username</p>
-                        <p className="light-text size-m">Suggested for you</p>
-                    </div>
-                </div>
-                <button className="follow-btn primary-text no-bg">Follow</button>
-            </div>
         </div>
     );
 };
