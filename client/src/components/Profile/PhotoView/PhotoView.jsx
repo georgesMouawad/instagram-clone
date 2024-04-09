02ac657c9bcf0395dab8b285b82a433d00d5d6a8
George <g.m.6@live.com>
2024-04-09 20:39:43 +0300

    photoviewer

--- client/src/components/Profile/PhotoView/PhotoView.jsx
+++ client/src/components/Profile/PhotoView/PhotoView.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 import './index.css';
 
-const PhotoView = () => {
+const PhotoView = ({}) => {
     const photo = {
         id: 2,
         image: '1712481515.jpg',
@@ -15,35 +18,43 @@ const PhotoView = () => {
     };
 
     return (
-        <div className="photo-view-main flex">
-            <div className="photo-view">
-                <img src={photo.image && `http://127.0.0.1:8000/posts/${photo.image}`} alt="post" />
+        <div className="photo-view-container black-bg-trsp flex space-between">
+            <div className="exit-btn white-text flex center">
+                {/* <FontAwesomeIcon icon={faXmark} onClick={setShowPhotoView(false)} /> */}
             </div>
-            <div className="comments-view flex column">
-                <div className="comments-view-header">
-                    <div className="comments-view-user flex">
-                        <img
-                            src={
-                                photo.user_image
-                                    ? `http://127.0.0.1:8000/profile-pictures/${photo.image}`
-                                    : './images/assets/avatar.png'
-                            }
-                            alt="user-avatar"
-                        />
-                        <p>{photo.username}</p>
-                    </div>
-                    <div className="comments-view-caption flex">
-                        <img
-                            src={
-                                photo.user_image
-                                    ? `http://127.0.0.1:8000/profile-pictures/${photo.image}`
-                                    : './images/assets/avatar.png'
-                            }
-                            alt="user-avatar"
-                        />
-                        <p>
-                            <span className='bold'>{photo.username}</span> {photo.caption}
-                        </p>
+            <div className="profile-view-nav flex center black-text white-bg">
+                <FontAwesomeIcon icon={faChevronLeft} />
+            </div>
+            <div className="photo-view-main white-bg flex">
+                <div className="photo-view">
+                    <img src={photo.image && `http://127.0.0.1:8000/posts/${photo.image}`} alt="post" />
+                </div>
+                <div className="comments-view flex column">
+                    <div className="comments-view-header">
+                        <div className="comments-view-user flex align-center bold size-m">
+                            <img
+                                src={
+                                    photo.user_image
+                                        ? `http://127.0.0.1:8000/profile-pictures/${photo.image}`
+                                        : './images/assets/avatar.png'
+                                }
+                                alt="user-avatar"
+                            />
+                            <p>{photo.username}</p>
+                        </div>
+                        <div className="comments-view-caption size-m flex center">
+                            <img
+                                src={
+                                    photo.user_image
+                                        ? `http://127.0.0.1:8000/profile-pictures/${photo.image}`
+                                        : './images/assets/avatar.png'
+                                }
+                                alt="user-avatar"
+                            />
+                            <p>
+                                <span className="bold">{photo.username}</span> {photo.caption}
+                            </p>
+                        </div>
                     </div>
                     <div className="comment-view flex">
                         <div className="comment-avatar flex center">
@@ -57,11 +68,25 @@ const PhotoView = () => {
                                 </p>
                             </div>
                             <div className="comment-info size-s">
-                                <p>1d</p>
+                                <p className="light-text">1d</p>
                             </div>
                         </div>
                     </div>
+                    <div className="photo-view-actions">
+                        
+                    </div>
+                    <div className="photo-view-post-details">
+                        <p className="photo-view-likes bold size-m"><span>1234</span> Likes</p>
+                        <p className="photo-view-date light-text size-m">1 day ago</p>
+                    </div>
+                    <div className="comment-add flex">
+                        <input type="text" placeholder="Add a comment..." />
+                        <button className='light-text'>Post</button>
+                    </div>
                 </div>
+            </div>
+            <div className="profile-view-nav flex center black-text white-bg">
+                <FontAwesomeIcon icon={faChevronRight} />
             </div>
         </div>
     );
